b81ac19ca919c388d347596361b653f308167be7
VaishnaviDK <vkolhapure99@gmail.com>
2016-04-22 20:47:05 +0530

    added updated form.php

--- form.php
+++ form.php
@@ -6,10 +6,10 @@
 <form method= "POST" action = "display.php"> 
 	Name : <input type = "text" name = "name">
 	
- 	Contact : <input type = "text" name = "contact" ">
+ 	Contact : <input type = "text" name = "contact">
  	
  	E-mail: <input type="text" name="email">
-   
+   	<p> hello world </p>
    <input type = "submit" name = "submit" value = "Submit">
  	</form>
  </body>
