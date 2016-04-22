62723ddc1b0b2a75579a7a87887ce79d67a7f973
VaishnaviDK <vkolhapure99@gmail.com>
2016-04-22 20:37:57 +0530

    added form.php

---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+<head> This is a simple form </head>
+
+<body> 
+<p> <span class = "error"> * required field </span><p>
+<form method= "POST" action = "display.php"> 
+	Name : <input type = "text" name = "name">
+	
+ 	Contact : <input type = "text" name = "contact" ">
+ 	
+ 	E-mail: <input type="text" name="email">
+   
+   <input type = "submit" name = "submit" value = "Submit">
+ 	</form>
+ </body>
+ </html>			
